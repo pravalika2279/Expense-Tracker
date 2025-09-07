91e2517c5f380e04cdc760577cce9b4a1261b140
Hanumantu Pravalika <hanumantupravalika@gmail.com>
2025-09-07 12:49:21 +0530

    Fix static files setup with Whitenoise and Render auto collectstatic

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -124,12 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS= [BASE_DIR/ "static", ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # required for Render
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] 
 
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
